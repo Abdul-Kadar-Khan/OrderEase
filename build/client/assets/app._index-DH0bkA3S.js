faaeb1cebaea7c5a66bea1b62eeb0898a678d19d
Abdul Kadar Khan <abdul.khan@galaxyweblinks.com>
2026-09-04 16:21:48 +0530

    d4

--- build/client/assets/app._index-DH0bkA3S.js
+++
@@ -1,1 +0,0 @@
-import{w as e,u as a,j as o,N as s}from"./jsx-runtime-CdoK6sCE.js";const i=e(function(){const t=a();return o.jsx(s,{to:`/app/active-services${t.search}`,replace:!0})});export{i as default};
